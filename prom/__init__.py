0c0f5fb59d46f5d7a18f263b591aa6fb13d22961
Jay Marcyes <jay@marcyes.com>
2016-09-14 17:51:19 -0700

    v0.9.92. Closes #33, closes #32

--- prom/__init__.py
+++ prom/__init__.py
@@ -12,7 +12,7 @@ from .utils import get_objects
 from .exception import InterfaceError, Error
 
 
-__version__ = '0.9.91'
+__version__ = '0.9.92'
 
 
 # get rid of "No handler found" warnings (cribbed from requests)
